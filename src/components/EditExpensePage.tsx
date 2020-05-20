86c8ef5676998b3a3b9740a7333beac830d63a64
Drew Fleming <dfleming@trackabout.com>
2020-05-20 17:21:17 -0400

    Chapter 9.8.

--- src/components/EditExpensePage.tsx
+++ src/components/EditExpensePage.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+type IdParams = { id: string };
 
-const EditExpensePage = () => (
-    <div>This is from my edit expense component.</div>
-);
+const EditExpensePage = (props: RouteComponentProps<IdParams>) => 
+{
+    console.log(props);
+    return (<div>Editing the expense with id {props.match.params.id}.</div>);
+}
 
 export default EditExpensePage;
