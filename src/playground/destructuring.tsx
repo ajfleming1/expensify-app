c2323c184b42c2c5733367099928cefc57a177fe
Drew Fleming <andrewfleming@gmail.com>
2020-05-23 19:53:43 -0400

    Chapter 10.7.

--- src/playground/destructuring.tsx
+++ src/playground/destructuring.tsx
@@ -1,4 +1,6 @@
-// export default {};
+export default {};
+
+// Object Destructuring 
 // type PersonType = {
 //     name?: string,
 //     age: number,
@@ -26,21 +28,30 @@
 //     console.log(`It's ${temperature} in ${city}.`);
 // }
 
-type BookType = {
-    title: string,
-    author: string,
-    publisher: {
-        name?: string
-    }
-}
+// type BookType = {
+//     title: string,
+//     author: string,
+//     publisher: {
+//         name?: string
+//     }
+// }
 
-const book:BookType = {
-    title: "Ego is the Enemy",
-    author: "Ryan Holiday",
-    publisher: {
-        name: "Penguin"
-    }
-}
+// const book:BookType = {
+//     title: "Ego is the Enemy",
+//     author: "Ryan Holiday",
+//     publisher: {
+//         name: "Penguin"
+//     }
+// }
 
-const {name: publisherName = "Self Published" } = book.publisher
-console.log(publisherName);
+// const {name: publisherName = "Self Published" } = book.publisher
+// console.log(publisherName);
+
+// Array Destructuring 
+// const address = ['123 Fake St', 'Salem', 'Virginia', '24201'];
+// const [, city, state = "New York"] = address;
+// console.log(`You are in ${city}, ${state}.`);
+
+const item = ["Coffee (iced)", "$3.00", "$3.50", "$3.75"];
+const [itemName, ,mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
