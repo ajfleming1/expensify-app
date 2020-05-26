4cc28222b3123289cc4c2d662b023373ac4d265a
Drew Fleming <andrewfleming@gmail.com>
2020-05-26 15:23:11 -0400

    Chapter 10.12.

--- src/playground/redux-expensify.tsx
+++ src/playground/redux-expensify.tsx
@@ -1,7 +1,8 @@
 import { createStore, combineReducers } from "redux";
 import { v4 as uuidv4 } from 'uuid';
 // Types
-type ExpenseActionTypes = AddExpenseAction | RemoveExpenseAction;
+type ExpenseActionTypes = AddExpenseAction | RemoveExpenseAction | EditExpenseAction;
+type FilterActionTypes = SetTextFilterAction;
 type ExepenseItemType = {
   id: string,
   description: string,
@@ -9,6 +10,7 @@ type ExepenseItemType = {
   amount: number,
   createdAt: number
 };
+
 type FiltersStateType = {
   text: string,
   sortBy: "amount" | "date",
@@ -16,9 +18,18 @@ type FiltersStateType = {
   endDate: Date
 };
 
+type UpdateExpenseType = {
+  description?: string, 
+  amount?: number, 
+  notes?: string, 
+  createdAt?: number
+}
+
 // Action Types
 const ADD_EXPENSE = "ADD_EXPENSE";
 const REMOVE_EXPENSE = "REMOVE_EXPENSE";
+const EDIT_EXPENSE = "EDIT_EXPENSE";
+const SET_TEXT_FILTER = "SET_TEXT_FILTER";
 type AddExpenseAction = {
   type: typeof ADD_EXPENSE,
   expense: ExepenseItemType
@@ -28,6 +39,18 @@ type RemoveExpenseAction = {
   type: typeof REMOVE_EXPENSE,
   id: string
 };
+
+type EditExpenseAction = {
+  type: typeof EDIT_EXPENSE,
+  id: string,
+  updates: UpdateExpenseType
+};
+
+type SetTextFilterAction = {
+  type: typeof SET_TEXT_FILTER,
+  text: string
+};
+
 
 // Default States
 const expensesDefaultState = [] as ExepenseItemType[];
@@ -63,7 +86,21 @@ const removeExpense = ({ id }: { id: string }): RemoveExpenseAction => (
 );
 
 // EDIT_EXPENSE
+const editExpense = ( {id, updates}: 
+  {id: string, updates: UpdateExpenseType}): EditExpenseAction => (
+  {
+    type: EDIT_EXPENSE,
+    id,
+    updates
+  }
+);
+
 // SET_TEXT_FILTER
+const setTextFilter = (text: string = ""): SetTextFilterAction => ({
+  type: SET_TEXT_FILTER,
+  text
+});
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -76,13 +113,29 @@ const expensesReducer = (state: ExepenseItemType[] = expensesDefaultState, actio
       return [...state, action.expense];
     case REMOVE_EXPENSE:
       return state.filter(({ id }) => id !== action.id)
+    case EDIT_EXPENSE:
+      return state.map((expense) => {
+        if(expense.id === action.id){
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        }
+      });
     default: return state;
   }
 };
 
 // Filters Reducer
-const filtersReducers = (state: FiltersStateType = filtersDefaultState, action: any): FiltersStateType => {
+const filtersReducers = (state: FiltersStateType = filtersDefaultState, action: FilterActionTypes): FiltersStateType => {
   switch (action.type) {
+    case SET_TEXT_FILTER: 
+      return {
+        ...state,
+        text: action.text
+      }
     default: return state;
   }
 };
@@ -116,6 +169,15 @@ store.dispatch(removeExpense({
   id: expenseOne.expense.id
 }));
 
+store.dispatch(editExpense({
+  id: expenseTwo.expense.id,
+  updates: {
+    amount: 500
+  }
+}));
+
+store.dispatch(setTextFilter("rent"));
+store.dispatch(setTextFilter());
 // const demoState = {
 //   expenses: [{
 //     id: "abcd",
@@ -131,3 +193,14 @@ store.dispatch(removeExpense({
 //     endDate: undefined as Date
 //   }
 // };
+
+// const user = {
+//   name: "Jen",
+//   age: 24
+// };
+
+// console.log({
+//   age: 27,
+//   ...user,
+//   location: "Bristol"
+// });
