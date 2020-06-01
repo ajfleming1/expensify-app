4841aa31d38ce705ccebaa4754f6e42d2c0c9ce8
Drew Fleming <andrewfleming@gmail.com>
2020-06-01 10:08:04 -0400

    Whitespace.

--- src/components/AddExpensePage.tsx
+++ src/components/AddExpensePage.tsx
@@ -5,15 +5,15 @@ import { UpdateExpenseType } from "../@types/expenseTypes";
 import { Props } from "../@types/rootState";
 import { addExpense } from "../actions/expenses";
 const AddExpensePage = (props: Props) => (
-    <div>
-        <h1>Add Expense </h1>
-        <ExpenseForm
-            onSubmit={(expense: UpdateExpenseType) => {
-                props.dispatch(addExpense(expense));
-                props.history.push("/");
-            }}
-        />
-    </div>
+  <div>
+    <h1>Add Expense </h1>
+    <ExpenseForm
+      onSubmit={(expense: UpdateExpenseType) => {
+        props.dispatch(addExpense(expense));
+        props.history.push("/");
+      }}
+    />
+  </div>
 );
 
 export default connect()(AddExpensePage);
