9be75ed13299fb28a12d29c29bc1d83d2704b3c7
Drew Fleming <dfleming@trackabout.com>
2020-06-03 14:03:35 -0400

    Chapter 11.12.

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,7 +1,7 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware  } from "redux";
 import expensesReducer from "../reducers/expenses";
 import filtersReducers from "../reducers/filters";
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 // Combine Reducers
 export default () => {
   const store = createStore(
@@ -9,7 +9,9 @@ export default () => {
       {
         expenses: expensesReducer,
         filters: filtersReducers
-      })
+      }),
+      
+      composeWithDevTools()
   );
 
   return store;
