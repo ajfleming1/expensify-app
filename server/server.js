e4b2984c5f742ac3562b164a9c08332fccd92267
Drew Fleming <andrewfleming@gmail.com>
2020-06-22 10:05:57 -0400

    Chapter 13.9.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var publicPath = path.join(__dirname, "..", "dist");
+var port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get("*", function (req, res) {
     res.sendFile(path.join(publicPath, "index.html"));
 });
-app.listen(3000, function () { return console.log("Server is up."); });
+app.listen(port, function () { return console.log("Server is up."); });
