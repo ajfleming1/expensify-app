7adb22681f6ffe34091458943e9d778992bbc22d
Drew Fleming <andrewfleming@gmail.com>
2020-08-06 20:25:39 -0400

    Chapter 17.2.

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 
-export const LoginPage = ({ startLogin }: {startLogin: (dispatch: any) => any}) => (
-  <div>
-    <button onClick={startLogin}>Login</button>
+export const LoginPage = ({ startLogin }: { startLogin: (dispatch: any) => any }) => (
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensify App</h1>
+      <p>Its time to get your expenses under control.</p>
+      <button onClick={startLogin}>Login</button>
+    </div>
   </div>
 );
 
