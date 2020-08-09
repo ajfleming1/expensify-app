21be98e7ffc9bc48ffccefe5f2a81833d2883aaf
Drew Fleming <andrewfleming@gmail.com>
2020-08-09 15:26:07 -0400

    Chapter 17.4.

--- src/components/ExpensesSummary.tsx
+++ src/components/ExpensesSummary.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import selectExpensesTotal from "../selectors/expenses-total";
 import { RootState } from "../@types/rootState";
 import selectExpenses from "../selectors/expenses";
@@ -8,10 +9,15 @@ import { connect } from "react-redux";
 export const ExpensesSummary = ({ expenseCount, expensesTotal }: { expenseCount: number, expensesTotal: number }) => {
   const expenseWord = expenseCount === 1 ? "expense" : "expenses";
   return (
-    <div>
-      <h1>
-        Viewing {expenseCount} {expenseWord} totalling {numerial(expensesTotal / 100).format("$0,0.00")}.
-      </h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">
+          Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{numerial(expensesTotal / 100).format("$0,0.00")}</span>.
+        </h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
